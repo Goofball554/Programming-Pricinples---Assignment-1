399f08d9e15fd7ae33742a56cf69c90ea03229d0
Goofball554 <123321081+Goofball554@users.noreply.github.com>
2023-01-22 14:00:11 -0500

    Added Comments

--- Dragon.py
+++ Dragon.py
@@ -1,4 +1,18 @@
+def Dragon(argument):
+    """
+    This module assigns the attribute values for the character of the Dragon.
+    """
+
+    return argument
+
+# Player health value is set to 10
 health = 10 
+
+# Attack attribute is set to 2
 attack = 2
+
+#Defense attribute is set to -2
 defense = -2
+
+#Magic attribute is set to 0
 magic = 0
