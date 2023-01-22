1b3a649d50e1e97810c15192246e47500488fc65
Goofball554 <123321081+Goofball554@users.noreply.github.com>
2023-01-22 13:59:48 -0500

    Added Comments

--- Fairy.py
+++ Fairy.py
@@ -1,4 +1,18 @@
-health = 10
+def Fairy(argument):
+    """
+    This module assigns the attribute values for the character of the Fairy.
+    """
+
+    return argument
+
+# Player health value is set to 10
+health = 10 
+
+# Attack attribute is set to -1
 attack = -1
+
+#Defense attribute is set to -1
 defense = -1
+
+#Magic attribute is set to 2
 magic = 2
